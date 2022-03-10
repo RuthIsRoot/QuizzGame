342b6ce9b532bde33f83f2738779e33817b81590
RuthIsRoot <98743413+RuthIsRoot@users.noreply.github.com>
2022-03-10 10:34:46 +0100

    Update quizzgame.sh

--- quizzgame.sh
+++ quizzgame.sh
@@ -4,39 +4,77 @@
 WHITE='\033[1;37m'
 GREEN='\033[1;32m'
 RED='\033[1;31m'
-BLUE='\00[1;34m'
+BOLD=$(tput bold)
+NORMAL=$(tput sgr0)
 NC='\033[0m' # No Color
 
 displayMenu(){
         JUGADOR=$(whoami)
-        echo -e "MENU DE OPCIONES\n=================\n"
+        echo -e "${WHITE}\nBIENVENIDO A TheQuizzGame ${GREEN}${JUGADOR}${NC} !\n${NC}"
+
+        echo -e "${WHITE}MENU DE OPCIONES\n=================\n${NC}"
 
         echo -e "1. Introducir preguntas/respuestas\n"
         echo -e "2. Jugar\n"
         echo -e "3. Ver puntuaciones\n"
         echo -e "4. Salir\n"
 
-        echo -e "Elige una opción"
+        echo -e "${WHITE}Elige una opción :${NC}"
         read opc
 
         case $opc in
                 1 | 1.)
-                        echo -e "\n[*] Introduce la nueva pregunta :"
-                        read PREGUNTA
-                        while [ -z $PREGUNTA ]; do
-                                echo -e "\n${RED}[!]${NC}No puedes dejar la pregunta vacia !"
-                                echo -e "\n[*] Introduce la nueva pregunta :"
+                        checkint='^[0-9]+$'
+                        x=0
+
+                        echo -e "\n${WHITE}[?] Cuantas preguntas quieres añadir ?${NC}"
+                        read NPREGUNTAS
+
+                        while ! [[ $NPREGUNTAS =~ $checkint ]]; do
+                                echo -e "\n${RED}[!]${NC} ${GREEN}${NPREGUNTAS}${NC} ${WHITE}no es un número de veces ! Introduce un número...${NC}"
+                                echo -e "\n${WHITE}[?] Cuantas preguntas quieres añadir ?${NC}"
+                                read NPREGUNTAS
+                        done
+
+                        while [ $x -lt $NPREGUNTAS  ]; do
+                                echo -e "\n${WHITE}[*] Introduce la nueva pregunta :${NC}"
                                 read PREGUNTA
-                        done
+                                while [[ -z $PREGUNTA ]]; do
+                                        echo -e "${RED}[!]${NC} ${WHITE}No puedes dejar la pregunta vacia !${NC}"
+                                        echo -e "\n${WHITE}[*] Introduce la nueva pregunta :${NC}"
+                                        read PREGUNTA
+                                done
 
-                        echo -e "\n[*] Introduce la respuesta a la pregunta anterior :"
-                        read RESPUESTA
-                        while [ -z $RESPUESTA ]; do
-                                echo -e "\n${RED}[!]${NC} No puedes dejar la respuesta vacia!"
-                                echo -e "\n[*] Introduce la respuesta a la pregunta anterior"
+                                echo -e "\n${WHITE}[*] Introduce la respuesta a la pregunta anterior :${NC}"
                                 read RESPUESTA
+                                while [[ -z $RESPUESTA ]]; do
+                                        echo -e "${RED}[!]${NC} ${WHITE}No puedes dejar la respuesta vacia!${NC}"
+                                        echo -e "\n${WHITE}[*] Introduce la respuesta a la pregunta anterior${NC}"
+                                        read RESPUESTA
+                                done
+
+                                echo -e "\n${GREEN}[*]${NC} ${WHITE}Añadiendo pregunta y respuesta...${NC}"
+                                echo $PREGUNTA >> preguntas.txt && echo $RESPUESTA >> respuestas.txt
+                                if [ $? -ne 0 ]; then
+                                        echo -e "\n${RED}[!] Algo ha fallado añadiendo tu pregutnas y respuesta !\n Saliendo...\n"
+                                        exit 1
+                                fi
+
+                                x=$(( $x + 1 ))
+
+                                if [ $x -ne $NPREGUNTAS ]; then
+                                        REST=$((NPREGUNTAS-x))
+                                        echo -e "${WHITE}[*] Todavia te quedan ${GREEN}${REST}${NC} ${WHITE}preguntas y respuestas para añadir${NC}"
+                                else
+                                        echo -e "\n${WHITE}[*] Ya has añadido tus ${GREEN}${NPREGUNTAS}${NC} ${WHITE}preguntas y respuestas !${NC}"
+                                fi
+
                         done
 
+                        echo -e "${BOLD}${GREEN}\n\tEspera 5 segundos para volver al menú.${NC}${NORMAL}"
+                        sleep 5
+                        clear
+                        displayMenu
                         ;;
 
                 2 |2.)
